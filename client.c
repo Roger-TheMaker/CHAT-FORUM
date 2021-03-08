f3f2f995d4afbe3bb3fc7632f75c19fe60d3eeba
dani <daniel.cernaianu@student.upt.ro>
2021-03-09 01:21:23 +0200

    all forum features implemented

--- client.c
+++ client.c
@@ -1,44 +1,284 @@
 #include <stdio.h>
+
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
+
 #include <string.h>
-#include <sys/socket.h>
+
+#include <signal.h>
+
+#include <unistd.h>
+
 #include <sys/types.h>
 
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
+
 #include <netinet/in.h>
 
+#include <arpa/inet.h>
+
+#include <pthread.h>
+
+#define SIZE 4096
+
+//global
+char user_name[32]; 
+char password[32];
+volatile sig_atomic_t flag = 0;
+int socket_fd=0;
+int wants_registration = 1; 
+
+//utility function for printing an arrow at the start of each line
+void print_arrow(){
+	printf("->");
+	fflush(stdout);
+}
+
+
+void sigint_handler(int signal)
+{
+	flag = 1;
+}
+
+void send_message_handler(void * arg)
+{
+	char message[SIZE] = "";
+	char user_name_and_message[SIZE+34] = "";
+
+	for(;;)
+	{
+		print_arrow();
+		fgets(message, SIZE, stdin);
+		message[strlen(message) - 1] = '\0';
+
+		if(!strcmp(message, "/exit"))
+		{
+			break;
+		}
+		else if(!strcmp(message, "/all"))
+		{
+			send(socket_fd, message, strlen(message), 0);
+		}
+		else
+		{
+			sprintf(user_name_and_message, "%s: %s\n", user_name, message);
+			send(socket_fd, user_name_and_message, strlen(user_name_and_message), 0);
+		}
+
+		memset(message, '\0', strlen(message));
+		memset(user_name_and_message, '\0', strlen(user_name_and_message));
+
+	}
+
+	sigint_handler(2);
+}
+
+void receive_message_handler(void * arg)
+{
+	char server_message[SIZE] = "";
+	for(;;)
+	{
+		int r;
+		if((r=recv(socket_fd, server_message, SIZE, 0)) > 0)
+		{
+			printf("%s", server_message);
+			print_arrow();
+		}
+		else if(r == 0)
+		{
+			break;
+		}
+
+		memset(server_message, '\0', strlen(server_message));
+	}
+
+	sigint_handler(2);
+}
+
+void registration(char * ip_address, int port_number)
+{
+	printf("REGISTRATION:\n");
+
+	printf("Username: ");
+	
+	getchar();
+
+	fgets(user_name, 32, stdin);
+	user_name[strlen(user_name)-1]='\0'; //remove endl character
+
+	printf("Password: ");
+
+	fgets(password, 32, stdin);
+	password[strlen(password)-1]='\0'; //remove endl character
+
+	struct sockaddr_in server_address; 
+
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(ip_address);
+	server_address.sin_port = htons(port_number);
+
+	int connection;
+
+	if((connection = connect(socket_fd, (struct sockaddr*) 
+		&server_address, sizeof(server_address))) == -1)
+	{
+		perror("Connection to server failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	send(socket_fd, &wants_registration, sizeof(int), 0);
+
+	send(socket_fd, user_name, sizeof(user_name), 0);
+
+	send(socket_fd, password, sizeof(password), 0);
+
+	pthread_t send_message_thread;
+
+	if((pthread_create(&send_message_thread, NULL, (void *)send_message_handler,NULL)) != 0)
+	{
+		perror("Message sender thread failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	pthread_t receive_message_thread;
+
+	if((pthread_create(&receive_message_thread, NULL, (void *)receive_message_handler, NULL)) != 0)
+	{
+		perror("Message receiver thread failed.\n");
+		exit(EXIT_FAILURE);
+	}	
+
+	for(;;)
+	{
+		if(flag){
+			printf("\nFarewell.\n");
+			break;
+		}
+	}
+
+	close(socket_fd);
+
+	exit(EXIT_SUCCESS);
+}
+
+void login(char * ip_address, int port_number)
+{
+	printf("LOGIN.\n");
+	printf("Username: ");
+	getchar();
+
+	fgets(user_name, 32, stdin);
+	user_name[strlen(user_name)-1]='\0';
+	
+	printf("Password: ");
+
+	fgets(password, 32, stdin);
+	password[strlen(password)-1]='\0';
+	
+	struct sockaddr_in server_address; 
+
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(ip_address);
+	server_address.sin_port = htons(port_number);
+
+	int connection;
+
+	if((connection = connect(socket_fd, (struct sockaddr*) 
+		&server_address, sizeof(server_address))) == -1)
+	{
+		perror("Connection to server failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	wants_registration = 0;
+
+	send(socket_fd, &wants_registration, sizeof(int), 0);
+
+	send(socket_fd, user_name, sizeof(user_name), 0);
+
+	send(socket_fd, password, sizeof(password), 0);
+
+	pthread_t send_message_thread;
+
+	if(pthread_create(&send_message_thread, NULL, (void *)send_message_handler,NULL) != 0)
+	{
+		perror("Message sender thread failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	pthread_t receive_message_thread;
+
+	if(pthread_create(&receive_message_thread, NULL, (void *)receive_message_handler, NULL) != 0)
+	{
+		perror("Message receiver thread failed.\n");
+		exit(EXIT_FAILURE);
+	}	
+
+	for(;;)
+	{
+		if(flag){
+			printf("\nFarewell.\n");
+			break;
+		}
+	}
+
+	close(socket_fd);
+
+	exit(EXIT_SUCCESS);
+
+}
+
+void menu(char *ip_address, int port_number)
+{
+
+  int option;
+
+  printf("== WELCOME TO THE FORUM ==\n");
+  printf("1.Registration\n");
+  printf("2.Login\n");
+  printf("3.Exit\n");
+  while (1) 
+  {
+
+  	printf("\nYour option:");
+  	scanf("%d", & option);
+
+    	switch (option) 
+    	{
+
+    	case 1:
+      	registration(ip_address, port_number);
+      	break;
+    	case 2:
+      	login(ip_address, port_number);
+      	break;
+    	case 3:
+      	exit(EXIT_FAILURE);
+      	break;
+    	default:
+      	printf("Wrong option!\n");
+
+    	}
+	}
+}
 
 int main(int argc, char **argv){
 
-//create a socket
-int network_socket;
-network_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if(argc != 2)
+	{
+		printf("Run program as such: ./%s <port_number>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-struct sockaddr_in server_address; 
-server_address.sin_family = AF_INET;
-server_address.sin_port = htons(9002);
-server_address.sin_addr.s_addr = INADDR_ANY;
+	char *ip_address = "0.0.0.0";
 
-int connection_status = connect(network_socket, 
-(struct sockaddr *) &server_address, 
-sizeof(server_address));
+	int port_number = atoi(argv[1]);
 
-//check for error with the connection
-if(connection_status == -1){
-   printf("There was an error making a connection to the remote socket\n\n");
-}
+	signal(SIGINT, sigint_handler);
 
-//recieve data from the server 
-char server_response[256];
-recv(network_socket, &server_response, 
-sizeof(server_response), 0);
+	menu(ip_address, port_number);
 
-printf("The server sent the data %s\n", server_response);
-
-close(network_socket);
 
 return 0;
 
--- server.c
+++ server.c
@@ -1,44 +1,386 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
 #include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include <errno.h>
+
+#include <string.h>
+
+#include <pthread.h>
+
 #include <sys/types.h>
 
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+#include <signal.h>
+
+#define MAX 100
+
+#define SIZE 4096
+
+#define FILE_NAME "dbase"
+
+//global
+static _Atomic unsigned int client_counter = 0;
+static int user_id = 0;
+FILE *dbase;
+int wants_registration;
+
+typedef struct{
+	struct sockaddr_in client_address;
+	int socket_fd;
+	int user_id;
+	char user_name[32];
+	char password[32];
+}client;
+
+client * client_array[MAX];
+
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+char * encrypt(char * string, int offset)
+{
+	for (int i = 0; i < strlen(string); i++) 
+	{
+    string[i] = string[i] + offset++;
+    }
+
+  	return string;
+}
+
+char * decrypt(char * string, int offset) 
+{
+
+  for (int i = 0; i < strlen(string); i++) 
+  {
+    string[i] = string[i] - offset++;
+  }
+
+  return string;
+}
+
+void print_client_address(struct sockaddr_in client_address)
+{
+	printf("%d.%d.%d.%d\n",
+		(client_address.sin_addr.s_addr & 0xFF),
+	    (client_address.sin_addr.s_addr & 0xFF00) >> 8,
+	    (client_address.sin_addr.s_addr & 0xFF0000) >> 16,
+	    (client_address.sin_addr.s_addr & 0xFF000000) >> 24);
+}
+
+void add_client_to_queue(client *instc_client)
+{
+	pthread_mutex_lock(&clients_mutex);
+
+	for(int i = 0; i < MAX; i++)
+	{
+		if(!client_array[i])
+		{
+			client_array[i] = instc_client;
+			break;
+		}
+	}
+	pthread_mutex_unlock(&clients_mutex);
+}
+
+void remove_client_from_queue(int user_id)
+{
+  
+  pthread_mutex_lock( & clients_mutex);
+
+  for (int i = 0; i < MAX; i++) 
+  {
+    if(client_array[i]) 
+    {
+      if(client_array[i] -> user_id == user_id) 
+      {
+        client_array[i] = NULL;
+        break;
+      }
+    }
+  }
+
+  pthread_mutex_unlock( & clients_mutex);
+
+}
+
+void send_message_to_other_clients(char * server_message, int user_id)
+{
+	pthread_mutex_lock(&clients_mutex);
+
+	for(int i = 0; i < MAX; i++)
+	{
+		if(client_array[i])
+		{
+			if(client_array[i] -> user_id != user_id)
+			{
+				if((write(client_array[i]->socket_fd, server_message, strlen(server_message))) < 0)
+				{
+					perror("Message sent incorrectly\n");
+					break;
+				}
+			}
+		}
+	}
+
+	pthread_mutex_unlock(&clients_mutex);
+}
+
+void send_message_to_sender(char * server_message, int user_id)
+{
+  pthread_mutex_lock(&clients_mutex);
+
+  for (int i = 0; i < MAX; ++i) 
+  {
+    if (client_array[i]) 
+    {
+      if (client_array[i] -> user_id == user_id) 
+      {
+        if (write(client_array[i] -> socket_fd, server_message, strlen(server_message)) < 0) 
+        {
+          perror("Message sent to sender failed.\n");
+          break;
+        }
+      }
+    }
+  }
+
+  pthread_mutex_unlock(&clients_mutex);
+}
+
+void write_credentials(char *user_name, char *password, client * instc_client)
+{
+  
+  dbase = fopen(FILE_NAME, "a");
+  char password_copy[32];
+  strcpy(password_copy, password);
+  fprintf(dbase, "%s %s\n", user_name, encrypt(password_copy, 3));
+  
+  fclose(dbase);
+
+}
+
+int find_credentials(char * user_name, char * password)
+{
+  char local_user_name[32];
+  char local_password[32];
+
+  dbase = fopen(FILE_NAME, "r");
+
+  while (!feof(dbase)) 
+  {
+    fscanf(dbase, "%s %s\n", local_user_name, local_password);
+    char local_password_copy[32];
+    strcpy(local_password_copy, local_password);
+    if (!strcmp(local_user_name, user_name) && !strcmp(decrypt(local_password_copy, 3), password)) 
+    {
+
+      fclose(dbase);
+      return 1;
+    }
+  }
+
+  fclose(dbase);
+  return 0;
+}
+
+void client_handler(void * arg)
+{
+
+	char server_message[SIZE];
+	char user_name[32];
+	char password[32];
+	int  exit_flag = 0;
+
+	client_counter++;
+
+	client * instc_client = (client *) arg;
+
+	recv(instc_client -> socket_fd, &wants_registration, sizeof(int), 0);
+
+	if(recv(instc_client -> socket_fd, user_name, 32, 0) <= 0 || strlen(user_name) > 32)
+	{
+		perror("Invalid username\n");
+		exit_flag = 1;
+	}
+
+	if(recv(instc_client -> socket_fd, password, 32, 0) <= 0 || strlen(password) < 6 || strlen(password) > 32)
+	{
+		perror("Invalid password\n");
+		exit_flag = 1;
+	}
+
+	if(!exit_flag)
+	{
+		if(wants_registration)
+		{
+			write_credentials(user_name, password, instc_client);
+		}
+		strcpy(instc_client->user_name, user_name);
+		strcpy(instc_client->password, password);
+		if(!find_credentials(instc_client->user_name, instc_client->password))
+		{
+			exit_flag = 1;
+		}
+		else
+		{
+			sprintf(server_message, "%s has joined.\n", instc_client->user_name);
+			printf("%s", server_message);
+			send_message_to_other_clients(server_message, instc_client->user_id);
+		}
+	}
+
+	memset(server_message, '\0', SIZE);
+
+	for(;;)
+	{
+		if(exit_flag)
+			break;
+	
+
+	int r;
+	if((r = recv(instc_client->socket_fd, server_message, SIZE, 0)) > 0)
+	{
+
+		if(!strcmp(server_message, "/all"))
+		{
+
+		char clients_list[10000];
+
+        strcpy(clients_list, "");
+
+        strcat(clients_list, "\n");
+
+        for (int i = 0; i < MAX; ++i) 
+        {
+
+          if(client_array[i]) 
+          {
+            strcat(clients_list, client_array[i] -> user_name);
+            strcat(clients_list, "\n");
+          }
+        }
+
+        send_message_to_sender(clients_list, instc_client -> user_id);
+		}
+		else if(strlen(server_message) > 0)
+		{
+			send_message_to_other_clients(server_message, instc_client->user_id);
+			server_message[strlen(server_message) - 1] = '\0';
+			printf("%s\n", server_message);
+		}
+	}
+	else if(r == 0)
+	{
+		sprintf(server_message, "%s has left.\n", instc_client->user_name);
+		printf("%s", server_message);
+		send_message_to_other_clients(server_message, instc_client->user_id);
+
+		exit_flag = 1;
+	}
+	else
+	{
+		perror("Receiving message failed.\n");
+		exit_flag = 1;
+	}
+
+	memset(server_message, '\0', SIZE);
+
+	}
 
 
-int main(){
+	close(instc_client->socket_fd);
+	remove_client_from_queue(instc_client->user_id);
+	free(instc_client);
+	client_counter--;
+	pthread_detach(pthread_self());
 
-char server_message[256] = "You have reached the server";
+}
 
-//create the server socket
-int server_socket; 
-server_socket = socket(AF_INET, SOCK_STREAM, 0);
+int main(int argc, char **argv)
+{
 
-//define the server address 
-struct sockaddr_in server_address; 
-server_address.sin_family = AF_INET;
-server_address.sin_port = htons(9002);
-server_address.sin_addr.s_addr=INADDR_ANY;
+	if(argc != 2)
+	{
+		printf("Run program as such: ./%s <port_number>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-//bind the socket to our specified IP and port
+	char * ip_address = "0.0.0.0";
 
-bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+	int port_number = atoi(argv[1]);
 
-listen(server_socket, 5);
+	int option = 1;
 
-int client_socket;
-client_socket = accept(server_socket, NULL, NULL);
+	int listen_fd = 0;
 
-//send the message 
-send(client_socket, server_message, sizeof(server_message), 0);
+	int connection_fd = 0;
 
-close(server_socket);
+	struct sockaddr_in server_address;
+
+	struct sockaddr_in client_address;
+
+	pthread_t client_thread;
+
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(ip_address);
+	server_address.sin_port = htons(port_number);
+
+	signal(SIGPIPE,SIG_IGN);
+
+	if((setsockopt(listen_fd,SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), (char*) &option, sizeof(option))) < 0)
+	{
+		perror("Error at setting socket option\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if((bind(listen_fd, (struct sockaddr*)&server_address, sizeof(server_address))) < 0)
+	{
+		perror("Error at binding socket\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if((listen(listen_fd, 10)) < 0)
+	{
+		perror("Error at socket listening\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("=== WELCOME TO THE FORUM ===\n");
+
+	for(;;)
+	{
+		socklen_t client_length = sizeof(client_address);
+		connection_fd = accept(listen_fd, (struct sockaddr*) & client_address, &client_length);
+
+		if((client_counter+1) == MAX)
+		{
+			printf("Number of clients exceeded.\n");
+			printf("Client address - ");
+			print_client_address(client_address);
+			printf(":%d\n", client_address.sin_port);
+			close(connection_fd);
+			continue;
+		}
+
+		client * instc_client = (client *) malloc(sizeof(client));
+		instc_client -> client_address = client_address;
+		instc_client -> socket_fd = connection_fd;
+		instc_client -> user_id = user_id++;
+
+		add_client_to_queue(instc_client);
+		pthread_create(&client_thread, NULL, (void*)client_handler, instc_client);
+	
+		sleep(1);
+	}
 
 return 0;
-
 }
